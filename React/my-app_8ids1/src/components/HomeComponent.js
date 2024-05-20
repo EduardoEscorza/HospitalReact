18a7dc753de89010e2993b800be1b3a039fdf89d
Eduardo Escorza <2520160124eescorzab@gmail.com>
2024-05-20 09:59:03 -0600

    Revert "videos funcionales"
    This reverts commit a45c2eafa3b9495d5cfd86d6c115427e2d232981.

--- React/my-app_8ids1/src/components/HomeComponent.js
+++ React/my-app_8ids1/src/components/HomeComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { DataGrid } from '@mui/x-data-grid';
 import axios from 'axios';
 import SpeedDial from '@mui/material/SpeedDial';
@@ -45,40 +45,42 @@ const fnNuevo = (params) => {
 
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'nombre', headerName: 'Nombre', width: 130 },
-    { field: 'nss', headerName: 'NSS', width: 130 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
     {
-      field: 'edad',
-      headerName: 'Edad',
+      field: 'age',
+      headerName: 'Age',
       type: 'number',
       width: 90,
     },
     {
-      field: 'domicilio',
-      headerName: 'Domicilio',
-      description: 'No se podra reordenar',
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
       sortable: false,
       width: 160,
       valueGetter: (params) =>
-        `${params.row.domicilio}`,
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
     },
   ];
 
   const getData = async () => {
-    const response = await axios.get('http://127.0.0.1:8000/api/pacientes').then((response) => {
+    const response = await axios.get('').then((response) => {
         console.log(response.data);
-        setRows(response.data);
     });
   }
 
-  const [rows, setRows] = useState([]);
-
-
-  useEffect(()=>{
-    console.log('Render');
-        getData();
-  },[]);
-
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
 
   return (
     <div align="center">
